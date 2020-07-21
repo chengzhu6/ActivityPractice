2a90490c28d5825675c9c81696989fff737450c6
qinghua <qinghua.liu@thoughtworks.com>
2020-07-21 15:08:23 +0800

    lifecycle calls are called when activity state change

--- app/src/main/java/com/thoughtworks/activity/LifeCycleActivity.java
+++ app/src/main/java/com/thoughtworks/activity/LifeCycleActivity.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class LifeCycleActivity extends AppCompatActivity {
 
@@ -10,5 +10,36 @@ public class LifeCycleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_life_cycle);
+        System.out.println("Create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("Start");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Destroy");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("Stop");
     }
 }
